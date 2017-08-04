95f9f24727cb2fbfd64341419458222d36d58e55
Tyson Hester <ironman9967@gmail.com>
2017-08-04 15:39:22 -0400

    attempted to get nvm to install only if it is out of date

--- src/hub/start-app.sh
+++ src/hub/start-app.sh
@@ -2,12 +2,18 @@
 
 touch ~/.bashrc
 
-rm -rf nvm_latest.json
+export NVM_CURRENT_VERSION=`nvm --version`
 
 export NVM_LATEST_VERSION=`curl -is https://api.github.com/repos/creationix/nvm/releases/latest | grep -o '"name": "[^"]*"' | grep -o 'v\d*\.\d*.\d*'`
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/$NVM_LATEST_VERSION/install.sh | bash
+if [ $NVM_CURRENT_VERSION != $NVM_LASTEST_VERSION ]; then
+	echo `updating nvm from $NVM_CURRENT_VERSION to $NVM_LASTEST_VERSION`
 
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+	curl -o- https://raw.githubusercontent.com/creationix/nvm/$NVM_LATEST_VERSION/install.sh | bash
+
+	export NVM_DIR="$HOME/.nvm"
+	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+else
+	echo `nvm $NVM_CURRENT_VERSION is up to date`
+fi
