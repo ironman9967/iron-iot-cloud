e3ad030a82e6e3c0bf8c13fb3cdc2d02e7460bd2
Tyson Hester <ironman9967@gmail.com>
2017-08-06 22:35:23 -0400

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo 'removing nvm'
-rm -rf ~/.nvm
-
 echo 'removing temp'
 rm -rf temp
 
@@ -12,33 +9,33 @@ ssh-keyscan github.com >> ~/.ssh/known_hosts
 echo 'cloning iron-iot-cloud'
 git clone git@github.com:ironman9967/iron-iot-cloud.git temp
 
-echo 'starting nvm'
-source ./common/scripts/start-nvm.sh
-
 echo 'navigating to temp'
 cd temp
+
+echo 'starting nvm'
+source ../common/scripts/start-nvm.sh
 
 echo 'npm install all deps'
 npm i
 
-echo 'building app'
-npm run build
+# echo 'building app'
+# npm run build
 
-echo 'navigate up from temp'
-cd ..
+# echo 'navigate up from temp'
+# cd ..
 
-echo 'copying dist'
-cp -r ./temp/dist ./
-echo 'copying package.json'
-cp ./temp/package.json ./
-echo 'copying package-lock.json'
-cp ./temp/package-lock.json ./
+# echo 'copying dist'
+# cp -r ./temp/dist ./
+# echo 'copying package.json'
+# cp ./temp/package.json ./
+# echo 'copying package-lock.json'
+# cp ./temp/package-lock.json ./
 
-echo 'removing temp'
-rm -rf temp
+# echo 'removing temp'
+# rm -rf temp
 
-echo 'removing node_modules'
-rm -rf node_modules
+# echo 'removing node_modules'
+# rm -rf node_modules
 
-echo 'npm install production deps'
-npm i --production
+# echo 'npm install production deps'
+# npm i --production
