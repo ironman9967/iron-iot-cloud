7b09bae2aa4cf2a16b3fcb0e0d78a935ea79de0d
Tyson Hester <ironman9967@gmail.com>
2017-08-07 10:23:41 -0400

    Update prep.sh

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -10,7 +10,7 @@ echo 'downloading latest cloud release'
 wget https://github.com/ironman9967/iron-iot-cloud/archive/latest.tar.gz
 
 echo 'extracting release'
-tar xvzf latest.tar.gz --transform 's/iron-iot-hub-latest/temp/'
+tar xvzf latest.tar.gz --transform 's/iron-iot-cloud-latest/temp/'
 
 echo 'removing release tar'
 rm latest.tar.gz
@@ -30,24 +30,24 @@ nvm use stable
 echo 'navigating to temp'
 cd temp
 
-# echo 'npm install all deps'
-# npm i
+echo 'npm install all deps'
+npm i
 
-# echo 'building app'
-# npm run build
+echo 'building app'
+npm run build
 
-# echo 'navigate up from temp'
-# cd ..
+echo 'navigate up from temp'
+cd ..
 
-# echo 'copying dist'
-# cp -r ./temp/dist ./
-# echo 'copying package.json'
-# cp ./temp/package.json ./
-# echo 'copying package-lock.json'
-# cp ./temp/package-lock.json ./
+echo 'copying dist'
+cp -r ./temp/dist ./
+echo 'copying package.json'
+cp ./temp/package.json ./
+echo 'copying package-lock.json'
+cp ./temp/package-lock.json ./
 
-# echo 'removing temp'
-# rm -rf temp
+echo 'removing temp'
+rm -rf temp
 
-# echo 'npm install production deps'
-# npm i --production
+echo 'npm install production deps'
+npm i --production
