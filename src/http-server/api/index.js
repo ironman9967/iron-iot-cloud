5f26aa5144f5beb5622583f258dac0ac465c9631
Tyson Hester <ironman9967@gmail.com>
2017-08-08 12:42:06 -0400

    modified start script and fixed some bad function names

--- src/http-server/api/index.js
+++ src/http-server/api/index.js
@@ -2,7 +2,7 @@
 import { createVersionsApi } from './bin/versions'
 
 export const routeApi = (server, devices) => {
-	const { versionsRoute } = createVersionsApi(devices)
-	server.route(versionsRoute())
+	const { createVersionsRoute } = createVersionsApi(devices)
+	server.route(createVersionsRoute())
 	return server
 }
