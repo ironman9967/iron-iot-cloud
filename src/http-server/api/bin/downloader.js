41a2cf4e38eb484b4699b47bde8141a465c683fd
Tyson Hester <ironman9967@gmail.com>
2017-08-13 03:48:18 -0400

    made prebuild GET endpoint

--- src/http-server/api/bin/downloader.js
+++ src/http-server/api/bin/downloader.js
@@ -1,4 +1,14 @@
 
+import { readdir } from 'fs'
+import { promisify } from 'util'
+import path from 'path'
+
+import map from 'lodash/fp/map'
+
+const fsReaddir = promisify(readdir)
+
+import { prebuildFolder } from '../../../bin-downloader'
+
 export const createBinDownloaderApi = ({
 	deviceUpsert
 }) => {
@@ -6,19 +16,33 @@ export const createBinDownloaderApi = ({
 
 	return {
 		createRoute: () => ({
-			method: 'POST',
+			method: [ 'GET','POST' ],
 			path: `/${apiRoute}/prebuild`,
 			handler: ({
-				payload: {
-					ref: version,
-					ref_type,
-					repository: { name: repo }
-				}
+				method,
+				payload
 			}, reply) => {
-				reply()
-				if (ref_type == 'tag' && version.indexOf('v') == 0)  {
-					const [ ,, model, iteration ] = repo.split('-')
-					deviceUpsert.next({ model, iteration })
+				if (method == 'get') {
+					fsReaddir(prebuildFolder)
+						.then(filenames =>
+							map(filename => {
+								const f = path.join(prebuildFolder, filename)
+								const p = 'public/'
+								return `/${f.substring(f.indexOf(p) + p.length)}`
+							})(filenames))
+						.then(reply)
+				}
+				else {
+					reply()
+					const {
+						ref: version,
+						ref_type,
+						repository: { name: repo }
+					} = payload
+					if (ref_type == 'tag' && version.indexOf('v') == 0)  {
+						const [ ,, model, iteration ] = repo.split('-')
+						deviceUpsert.next({ model, iteration })
+					}
 				}
 			}
 		})
