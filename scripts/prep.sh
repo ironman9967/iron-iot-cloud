c09395a78dcc528c75c012b2fadd1d4fa4822f3d
Tyson Hester <ironman9967@gmail.com>
2017-08-21 15:10:50 -0400

    getting get-app from common instead of cloud repo

--- scripts/prep.sh
+++ scripts/prep.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#test
-
 model=$1
 
 source $APP_PATH/common/scripts/start-nvm.sh
@@ -16,7 +14,7 @@ echo 'setting nvm to use stable'
 nvm use stable
 
 echo "getting app for $model"
-wget -O $APP_PATH/get-app.sh "https://raw.githubusercontent.com/ironman9967/iron-iot-cloud/master/scripts/get-app.sh"
+wget -O $APP_PATH/get-app.sh "https://raw.githubusercontent.com/ironman9967/iron-iot-common/master/scripts/get-app.sh"
 source $APP_PATH/get-app.sh $APP_PATH cloud
 
 source $APP_PATH/common/scripts/build-app.sh $version cloud
