8f094462d0dc713bb46c77af08186b81534267cc
Tyson Hester <ironman9967@gmail.com>
2017-08-27 11:02:12 -0400

    WIP github webhook secret verification

--- src/http-server/api/github/webhook.js
+++ src/http-server/api/github/webhook.js
@@ -12,7 +12,7 @@ export const createGithubWebhookApi = ({
 	}) => {
 		reply()
 		const signature = req.headers['X-Hub-Signature']
-		logger.next('X-Hub-Signature', { signature })
+		logger.next('headers', { headers: req.headers })
 		const { ref, ref_type, repository } = req.payload
 		if (ref_type == 'tag' && ref.indexOf('v') == 0) {
 			const { name: repo } = repository
