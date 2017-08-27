918ce0e07c3ce5429c773915390122399b38817e
Tyson Hester <ironman9967@gmail.com>
2017-08-27 09:54:05 -0400

    added logging when github posts something other than a release tag

--- src/http-server/api/github/webhook.js
+++ src/http-server/api/github/webhook.js
@@ -1,51 +1,44 @@
 
-import { Subject } from '@reactivex/rxjs/dist/cjs/Subject'
-
-const parseTag = ({
-	tagParsed,
-	payload,
-	reply
-}) => {
-	reply()
-	const { ref, ref_type } = payload
-	if (ref_type == 'tag' && ref.indexOf('v') == 0) {
-		tagParsed.next(payload)
-	}
-}
-
 export const createGithubWebhookApi = ({
 	logger,
 	deviceUpsert,
 	selfUpdateReady
 }) => {
-	const tagParsed = new Subject()
-	tagParsed.subscribe(({
-		ref: version,
-		ref_type,
-		repository: { name: repo }
+	const parseTag = ({
+		payload,
+		reply
 	}) => {
-		const [ ,, model, iteration ] = repo.split('-')
-		logger.next([ 'release posted', {
-			model,
-			iteration,
-			version,
-			repo
-		}])
-		if (model != 'cloud' && iteration) {
-			const d = { model, iteration }
-			logger.next([ 'prebuild ready', d ])
-			deviceUpsert.next(d)
+		reply()
+		const { ref, ref_type } = payload
+		if (ref_type == 'tag' && ref.indexOf('v') == 0) {
+			const [ ,, model, iteration ] = repo.split('-')
+			logger.next([ 'release posted', {
+				model,
+				iteration,
+				version,
+				repo
+			}])
+			if (model != 'cloud' && iteration) {
+				const d = { model, iteration }
+				logger.next([ 'prebuild ready', d ])
+				deviceUpsert.next(d)
+			}
+			else {
+				selfUpdateReady.next()
+			}
 		}
 		else {
-			selfUpdateReady.next()
+			logger.next([ 'non-tag post received from github', {
+				ref,
+				ref_type
+			}])
 		}
-	})
+	}
 	return {
 		createRoute:() => ({
 			method: 'POST',
 			path: '/api/github/webhook/{event}',
 			handler: ({ payload }, reply) => parseTag({
-				tagParsed,
 				payload,
 				reply
 			})
